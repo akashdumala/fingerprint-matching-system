763ac832f8bf273ae616cc5d81f18b1b7741a30c
akash_dumala <akashxpatel999@gmail.com>
2026-08-04 15:45:03 +0530

    Test matcher import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import tempfile
 
 from matcher.matcher_engine import match_images
 
@@ -10,4 +11,19 @@ st.set_page_config(
 
 st.title("Fingerprint Matching System")
 
-st.success("Imports successful!")
+image1 = st.file_uploader("Upload Fingerprint 1", type=["tif"])
+image2 = st.file_uploader("Upload Fingerprint 2", type=["tif"])
+
+if image1 and image2:
+
+    st.success("Files uploaded successfully!")
+
+    temp1 = tempfile.NamedTemporaryFile(delete=False, suffix=".tif")
+    temp1.write(image1.read())
+    temp1.close()
+
+    temp2 = tempfile.NamedTemporaryFile(delete=False, suffix=".tif")
+    temp2.write(image2.read())
+    temp2.close()
+
+    st.success("Temporary files created successfully!")
